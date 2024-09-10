345e59cfc41f98e28b170a994a2a49cc03752cab
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-09-10 18:26:37 +0200

    Attach shadow dom to the menu component

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,13 +1,15 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: "open" });
   }
 
   // When the component is attached to the dom
   connectedCallback() {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
-    this.appendChild(content);
+    this.root.appendChild(content);
   }
 }
 
